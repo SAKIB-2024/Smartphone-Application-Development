2f6ba588fa1effbcb2e0c4541f9702d44ce371e8
MAHMUDUL HASAN SAKIB <157484516+SAKIB-2024@users.noreply.github.com>
2025-10-11 14:33:15 +0600

    Create Que_10.dart

---
+++ Dart Basic/Practice 01/Que_10.dart
@@ -0,0 +1,11 @@
+// Write a Dart program to convert String to int.
+import 'dart:io';
+
+void main() {
+  String str = "123";
+  int number = int.parse(str);
+
+  print("String: $str");
+  print("Converted to int: $number");
+}
+
