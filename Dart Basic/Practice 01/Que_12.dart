b02c6f19b394e8fc66751123aed46a47a5ed95a7
MAHMUDUL HASAN SAKIB <157484516+SAKIB-2024@users.noreply.github.com>
2025-10-11 14:44:13 +0600

    Create Que_12.dart

---
+++ Dart Basic/Practice 01/Que_12.dart
@@ -0,0 +1,11 @@
+// Suppose, your distance to office from home is 25 km and you travel 40 km per hour. 
+// Write a program to calculate time taken to reach office in minutes. Formula= (distance) / (speed)
+import 'dart:io';
+
+void main() {
+    int distance = 25;
+    int velocity = 40;
+    double time = (25 /40) * 60;
+    print("Time need : $time minutes");
+}
+
