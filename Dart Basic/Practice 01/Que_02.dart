da770df3d310127e8ca0f23f846bf20e05ae6f85
MAHMUDUL HASAN SAKIB <157484516+SAKIB-2024@users.noreply.github.com>
2025-10-11 13:22:47 +0600

    Update Que_02.dart

--- Dart Basic/Practice 01/Que_02.dart
+++ Dart Basic/Practice 01/Que_02.dart
@@ -1,9 +1,8 @@
 // Write a program to print Hello I am “John Doe” and Hello I’am “John Doe” with single and double quotes.
-void main(){
+void main() {
   //single quotes
   print('Hello I am "John Doe"');
   print( 'Hello I\'am "John Doe"');
-
   //Double qutes
   print("Hello I am \"John Doe\"");
   print( "Hello I'am \"John Doe\"");
